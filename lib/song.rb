fd8339c8685955607fb02a1e0e554177720b3f03
Brad Schrag <brad.schrag@gmail.com>
2018-11-13 00:55:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,16 @@ class Song
     @@all
   end
 
+  def self.create
+    @@all << self.new
+    @@all[-1]
+  end
+
+  def self.new_by_name(name)
+    new_song = self.create
+    new_song.name = name
+    new_song
+
   def save
     self.class.all << self
   end
